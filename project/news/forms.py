396beb076a3cbf1284f5ecb55c88f41974562fa4
Bulatr <bulat_r@inbox.ru>
2023-06-22 23:06:46 +0800

    работа с поиском

---
+++ project/news/forms.py
@@ -0,0 +1,4 @@
+from django import forms
+
+class SearchForm(forms.Form):
+    q = forms.CharField(label='Поиск', widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Введите текст...'}))
--- project/news/urls.py
+++ project/news/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import PostList, PostDetail
+from .views import PostList, PostDetail, search_view
 
 app_name = 'news'
 
 urlpatterns = [
     path('', PostList.as_view(), name='post_list'),
     path('<int:pk>/', PostDetail.as_view(), name='post_detail'),
+    path('search/', search_view, name='search_view'),
 ]
