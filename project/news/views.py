29083782030a696d4405506aa02f254bb35b2763
Bulatr <bulat_r@inbox.ru>
2023-06-23 21:34:33 +0800

    Реализован поиск

--- project/news/views.py
+++ project/news/views.py
@@ -3,6 +3,8 @@ from django.views.generic import ListView, DetailView
 from .models import Post, Category
 from .forms import SearchForm
 from .filters import PostFilter
+from django.utils import timezone
+from datetime import datetime
 
 # Create your views here.
 class PostList(ListView):
@@ -30,8 +32,26 @@ class PostSearchView(ListView):
     context_object_name = 'posts'
 
     def get_queryset(self):
+        queryset = super().get_queryset()
         query = self.request.GET.get('q')
+        category_id = self.request.GET.get('category')
+        date = self.request.GET.get('date')
         if query:
-            queryset = Post.objects.filter(title__icontains=query)
-            return queryset
-        return Post.objects.all()
+            # queryset = Post.objects.filter(title__icontains=query)
+            queryset = queryset.filter(title__icontains=query)
+        if category_id:
+            queryset = queryset.filter(category=category_id)
+        if date:
+            start_date = timezone.make_aware(datetime.strptime(date, '%Y-%m-%dT%H:%M'))
+            end_date = start_date.replace(hour=23, minute=59, second=59)
+            queryset = queryset.filter(create_time__range=(start_date, end_date))
+        return queryset
+
+    # Чтобы вывести список всех категорий в форме, нужно передать список категорий из представления
+    # в контекст шаблона. Затем вы можете использовать этот список в шаблоне для создания элементов
+    # <option> в выпадающем списке
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['categories'] = Category.objects.all()
+        return context
